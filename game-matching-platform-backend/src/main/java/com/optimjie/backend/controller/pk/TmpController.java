fa91c8d581a8a852c929a44506adfcb1440176ab
optimjie <1623287075@qq.com>
2022-07-08 16:24:48 +0800

    初始化前后端

---
+++ game-matching-platform-backend/src/main/java/com/optimjie/backend/controller/pk/TmpController.java
@@ -0,0 +1,19 @@
+package com.optimjie.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk")
+public class TmpController {
+
+    @RequestMapping("/getInfo")
+    public Map<String, String> getInfo() {
+        Map<String, String> mp = new HashMap<>();
+        mp.put("name", "Tom");
+        return mp;
+    }
+}
---
+++ game-matching-platform-frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>Bot昵称：{{ bot_name }}</div>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+
+    $.ajax({
+      url: "http://localhost:9090/pk/getInfo",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+      }
+    });
+
+    return {
+      bot_name
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url("assets/background.png");
+  background-size: cover;
+}
+</style>
